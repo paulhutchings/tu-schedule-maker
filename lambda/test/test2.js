29bc8d2c33931505b218f6c1e321af13c8a5b88c
paulhutchings <36493057+paulhutchings@users.noreply.github.com>
2019-01-01 21:12:39 -0500

    removed test dir to fix gitignore issues

--- lambda/test/test2.js
+++
@@ -1,7 +0,0 @@
-var map = new Map();
-
-map.set('foo', 'bar');
-map.set(1, 2);
-map.set('key', 'value');
-
-Array.from(map.entries()).forEach(entry => console.log(`Key: ${entry[0]}\nValue: ${entry[1]}`));
