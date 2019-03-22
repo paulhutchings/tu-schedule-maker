e7dfe734c336967f1d4c2a4ef8ab0be6e99528ab
paulhutchings <36493057+paulhutchings@users.noreply.github.com>
2019-03-21 22:45:42 -0400

    modified updateClassesMain to use object freezing

--- lambda/updateClassesMain/index.js
+++ lambda/updateClassesMain/index.js
@@ -1,18 +1,30 @@
-//main Lambda that delegates each subject to a worker lambda
+/**
+ * @description - Main Lambda that delegates each subject to a worker lambda
+ */
 const {S3Util} = require('./modules/aws-utils');
 const AWS = require('aws-sdk');
-const Lambda = new AWS.Lambda();
 const env = process.env;
 
-//initialize s3 and read subjects.JSON
-const s3 = new S3Util(env.bucket);
-const subjectsPromise = s3.read(env.subjectsJSON);
-let subjects = null; //declare here so that it persists between invocations
+//Create Lambda object if it doesn't exist from a previous invocation
+if (!lambda){
+    var lambda = new AWS.Lambda();
+}
 
+//Declare subjects array if it doesn't exist from a previous invocation
+if (!subjects){
+    var subjects = null; //Initialized in handler
+}
+
+/**
+ * @exports
+ */
 exports.handler = async(event) => {   
     try {
-        subjects = await subjectsPromise;
-        let pending = subjects.map(subject => delegate(subject)); //will hold pending lambda invocations
+        //Load list of subjects
+        if (!subjects){
+            subjects = await load_subjects();
+        }
+        let pending = subjects.map(subject => delegate(subject.id)); //will hold pending lambda invocations
         await Promise.all(pending);
         return 'Success';
     } catch (error) {
@@ -20,19 +32,34 @@ exports.handler = async(event) => {
     }
 };
 
-//invokes the worker lambda to process process the given subject
+/**
+ * @async
+ * @function delegate - Invokes the worker lambda to process process the given subject
+ * @param {string} subject 
+ */
 async function delegate(subject){
     const params = {
         'FunctionName': env.lambda,
         'InvocationType': 'Event',
         'Payload': JSON.stringify({
-            'subject': subject.id
+            'subject': subject
         })
     };
     try {
         //invoke worker lambda
-        return await Lambda.invoke(params).promise();
+        return await lambda.invoke(params).promise();
     } catch (error) {
         return error;
     }
+}
+
+/**
+ * @async
+ * @function load_subjects - Reads the subjects.json file from s3
+ * @returns {[object]}
+ */
+async function load_subjects(){
+    //initialize s3 and read subjects.JSON
+    const s3 = new S3Util(env.bucket);
+    return JSON.parse(await s3.read(env.subjectsJSON));
 }
